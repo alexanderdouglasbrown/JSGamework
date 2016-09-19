02c4c50bc7678b80265e5e34f82b86fd0c163302
Alex <whogie@gmail.com>
2016-09-19 16:58:09 -0700

    Basic resizable canvas

---
+++ main.js
@@ -0,0 +1,25 @@
+var global = {
+    canvas : document.createElement("canvas")
+}
+
+function initialize(){
+    global.canvas.width = window.innerWidth;
+    global.canvas.height = window.innerHeight;
+    global.context = global.canvas.getContext("2d");
+    document.body.insertBefore(global.canvas, document.body.childNodes[0]);
+
+}
+
+function draw(){
+    global.context.fillStyle="blue";
+    global.context.fillRect(0,0,global.canvas.width,global.canvas.height);
+}
+
+//Look for resize and redraw
+window.addEventListener("resize", resizeCanvas, false);
+
+function resizeCanvas(e) {
+    global.canvas.width = window.innerWidth;
+    global.canvas.height = window.innerHeight;
+    draw();
+}
