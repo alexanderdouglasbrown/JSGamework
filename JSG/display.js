24dfaf7be8ac974738e32b18945b7c7964d24250
Alex Brown <alexanderdouglasbrown@gmail.com>
2017-02-23 23:57:15 -0800

    Clean up redundant code in display

--- JSG/display.js
+++ JSG/display.js
@@ -11,50 +11,30 @@ function JSG_createCanvas(width, height) {
     JSG.resolutionHeight = height
 
     JSG.canvas = document.createElement("canvas")
-    if ((window.innerHeight * (JSG.resolutionWidth / JSG.resolutionHeight)) >= (window.innerWidth)) {
-        JSG.canvas.width = window.innerWidth
-        JSG.canvas.height = window.innerWidth * (JSG.resolutionHeight / JSG.resolutionWidth)
-
-        JSG.context = JSG.canvas.getContext("2d")
-        document.body.insertBefore(JSG.canvas, document.body.childNodes[0])
-
-        JSG.display.widthScale = window.innerWidth / JSG.resolutionWidth
-        JSG.display.heightScale = window.innerWidth / JSG.resolutionWidth
-        JSG.context.scale(JSG.display.widthScale, JSG.display.heightScale)
-    } else {
-        JSG.canvas.width = window.innerHeight * (JSG.resolutionWidth / JSG.resolutionHeight)
-        JSG.canvas.height = window.innerHeight
-
-        JSG.context = JSG.canvas.getContext("2d")
-        document.body.insertBefore(JSG.canvas, document.body.childNodes[0])
-
-        JSG.display.widthScale = window.innerHeight / JSG.resolutionHeight
-        JSG.display.heightScale = window.innerHeight / JSG.resolutionHeight
-        JSG.context.scale(JSG.display.widthScale, JSG.display.heightScale)
-    }
+    document.body.insertBefore(JSG.canvas, document.body.childNodes[0])
+    JSG.context = JSG.canvas.getContext("2d")
+    JSG_resizeCanvas()
 }
 
-//Look for resize and redraw
-window.addEventListener("resize", JSG_resizeCanvas)
-window.addEventListener("orientationchange", JSG_resizeCanvas)
-
-function JSG_resizeCanvas(e) {
-    if ((window.innerHeight * (JSG.resolutionWidth / JSG.resolutionHeight)) >= (window.innerWidth)) {
+function JSG_resizeCanvas() {
+    if (window.innerHeight * (JSG.resolutionWidth / JSG.resolutionHeight) >= window.innerWidth) {
         JSG.canvas.width = window.innerWidth
         JSG.canvas.height = window.innerWidth * (JSG.resolutionHeight / JSG.resolutionWidth)
 
         JSG.display.widthScale = window.innerWidth / JSG.resolutionWidth
         JSG.display.heightScale = window.innerWidth / JSG.resolutionWidth
-        JSG.context.scale(JSG.display.widthScale, JSG.display.heightScale)
     } else {
         JSG.canvas.width = window.innerHeight * (JSG.resolutionWidth / JSG.resolutionHeight)
         JSG.canvas.height = window.innerHeight
 
         JSG.display.widthScale = window.innerHeight / JSG.resolutionHeight
         JSG.display.heightScale = window.innerHeight / JSG.resolutionHeight
-        JSG.context.scale(JSG.display.widthScale, JSG.display.heightScale)
     }
+    JSG.context.scale(JSG.display.widthScale, JSG.display.heightScale)
 
     //Ignore inputs when resizing
     JSG.keyboard.keyList = []
 }
+
+window.addEventListener("resize", JSG_resizeCanvas)
+window.addEventListener("orientationchange", JSG_resizeCanvas)
